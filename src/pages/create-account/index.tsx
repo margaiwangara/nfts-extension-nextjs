fec4993a32538de8561aaa19b7fc121d2968f00d
margaiwangara <margaiwangara@gmail.com>
2022-01-30 01:52:32 +0300

    validation: create account page form validation implemented

--- src/pages/create-account/index.tsx
+++ src/pages/create-account/index.tsx
@@ -1,9 +1,8 @@
-import AuthLayout from '@containers/AuthLayout';
-import { Button, Input, AuthBody, IntroText, Label } from '@components/ui';
-import styled from 'styled-components';
-import { Formik, Form } from 'formik';
-import Link from 'next/link';
 import LoginWithNear from '@components/shared/LoginWithNear';
+import { AuthBody, IntroText } from '@components/ui';
+import AuthLayout from '@containers/AuthLayout';
+import styled from 'styled-components';
+import CreateAccountForm from './components/CreateAccountForm';
 
 function CreateAccount() {
   return (
@@ -16,36 +15,7 @@ function CreateAccount() {
             receiving assets.
           </CustomIntroText>
         </section>
-        <Formik initialValues={{}} onSubmit={() => {}}>
-          {({}) => (
-            <Form className="form">
-              <FormGroup>
-                <Label htmlFor="fullName">Full Name</Label>
-                <Input type="text" placeholder="Ex. John Doe" id="fullName" />
-              </FormGroup>
-              <FormGroup>
-                <Label htmlFor="accountId">Account ID</Label>
-                <section className="input-group">
-                  <CustomInput type="text" placeholder="yourname" />
-                  <button className="readonly-btn">.near</button>
-                </section>
-              </FormGroup>
-              <Button style={{ alignSelf: 'center' }} disabled>
-                Continue
-              </Button>
-              <p className="agreement">
-                By creating a NEAR account, you agree to the NEAR Wallet{' '}
-                <Link href="#terms-of-service">
-                  <a>Terms of Service</a>
-                </Link>{' '}
-                and{' '}
-                <Link href="#privacy-policy">
-                  <a>Privacy Policy</a>
-                </Link>
-              </p>
-            </Form>
-          )}
-        </Formik>
+        <CreateAccountForm />
         <div className="line" />
         <LoginWithNear />
       </CustomAuthBody>
@@ -59,33 +29,6 @@ const CustomAuthBody = styled(AuthBody)`
   }
 `;
 
-const FormGroup = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  width: 100%;
-
-  .input-group {
-    display: grid;
-    grid-template-columns: 1fr 81px;
-
-    .readonly-btn {
-      border-radius: 0 10px 10px 0;
-      background-color: transparent;
-      border: solid 1px ${({ theme }) => theme.colors.text[500]};
-      padding: 11.5px 20px;
-      font-weight: 500;
-      font-size: 16px;
-      color: #000000;
-      text-align: center;
-    }
-  }
-`;
-
-const CustomInput = styled(Input)`
-  border-radius: 10px 0 0 10px;
-  border-right-color: transparent;
-`;
 const CustomIntroText = styled(IntroText)`
   color: #808080;
 `;
