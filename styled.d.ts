e4091adc2cffb113229fe8e3383104e74383d9f3
margaiwangara <margaiwangara@gmail.com>
2022-01-29 12:01:15 +0300

    init: added theme values to styled-components

--- styled.d.ts
+++ styled.d.ts
@@ -1,7 +1,31 @@
 import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
-    colors: {};
+    colors: {
+      blue: {
+        100: string;
+        200: string;
+        300: string;
+        400: string;
+        500: string;
+      };
+      accent: {
+        1: string;
+        2: string;
+        3: string;
+      };
+      text: {
+        100: string;
+        200: string;
+        300: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+      };
+      danger: string;
+    };
     breakpoints: {
       sm: string;
     };
